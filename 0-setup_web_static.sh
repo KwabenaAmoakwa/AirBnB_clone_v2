8fbff859949d7cbf36177ae2fdbe6b14f1111c0a
Jewelz_B <jewelbansah@rocketmail.com>
2024-02-02 11:47:19 +0000

    fix task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,7 @@ sudo echo "Test My Server" | sudo tee /data/web_static/releases/test/index.html
 
 # Create a new symbolic link
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
-sudo chmod -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
