4f1a70d675e41b49acbc06c26fc08a95e2d70262
Jewelz_B <jewelbansah@rocketmail.com>
2024-02-02 14:18:40 +0000

    Fix task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static. It must:
-
+sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get install -y nginx
 sudo ufw allow 'Nginx HTTP'
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-sudo echo "Test My Server" | sudo tee /data/web_static/releases/test/index.html
+echo "Test My Server" | sudo tee /data/web_static/releases/test/index.html
 
 # Create a new symbolic link
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
-
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '/listen 80 default_server/a \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
