653f26af0a875aae5c16be723ef373db1051a03c
Jewel Bansah <jewelbansah@rocketmail.com>
2024-02-04 10:50:55 +0000

    fix task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,9 +4,13 @@ sudo apt-get -y update
 sudo apt-get install -y nginx
 sudo ufw allow 'Nginx HTTP'
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "Test My Server" | sudo tee /data/web_static/releases/test/index.html
-
-# Create a new symbolic link
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/listen 80 default_server/a \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
